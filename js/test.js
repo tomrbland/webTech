6eb232750c50c184bb8ef5569554eeb4abe4781d
AK <ak15308@my.bristol.ac.uk>
2016-02-18 12:19:58 +0000

    I forgot to commit some stuff

--- js/test.js
+++ js/test.js
@@ -1,19 +1,20 @@
-var today = new Date();
-var hourNow = today.getHours();
-var minNow = today.getMinutes();
+   var today = new Date();
+   var hourNow = today.getHours();
+   var minNow = today.getMinutes();
 
-// can't be bothered with css
-document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+   for (i = 0; i < 44; i++) {
+      document.write("&nbsp;");
+   }
 
-if ((hourNow < 10) & (minNow < 10)) {
-   document.write("The time is 0" + hourNow + ":0" + minNow + ".");
-}
-else if (hourNow < 10) {
-   document.write("The time is 0" + hourNow + ":" + minNow + ".");
-}
-else if (minNow < 10) {
-   document.write("The time is " + hourNow + ":0" + minNow + ".");
-}
-else {
-   document.write("The time is " + hourNow + ":" + minNow + ".");
-}
+   if ((hourNow < 10) & (minNow < 10)) {
+      document.write("The time is 0" + hourNow + ":0" + minNow + "");
+   }
+   else if (hourNow < 10) {
+      document.write("The time is 0" + hourNow + ":" + minNow + "");
+   }
+   else if (minNow < 10) {
+      document.write("The time is " + hourNow + ":0" + minNow + "");
+   }
+   else {
+      document.write("The time is " + hourNow + ":" + minNow + "");
+   }
