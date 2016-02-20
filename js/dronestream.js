e742171586f7a94408e4a87f2e34c1bc158a884e
AK <ak15308@my.bristol.ac.uk>
2016-02-20 20:57:33 +0000

    Managed to parse a json file (test.json) but still can't get it to work for the dronestream data.

--- js/dronestream.js
+++ js/dronestream.js
@@ -1,13 +1,29 @@
 
-   document.write('<p>' + "Test." + '</p>');
-
    var data={ "firstName" : "Hi Tom" };
    alert(data.firstName);
 
-/*
-   This is the correct url for the dronestream
-   var url = api.dronestre.am/data
 
-   I think we'll need do this at some point.  
-   var obj = JSON.parse(text);
-*/
+   //The beginning of my attemp to parse the JSON data
+   var url = "api.dronestre.am/data";
+
+   var xhr = new XMLHttpRequest();
+
+   xhr.onload = function() {
+      if (xhr.status === 200) {
+         document.write("<p>" + "Response check." + "</p>");
+         responseObject = JSON.parse(xhr.responseText);
+
+         // Build up the string with new content
+         var newContent = " ";
+         for (var i = 0; i < responseObject.strike.length; i++) {
+            newContent += "<p>" + responseObject.strike[i].country + "</p>";
+            document.write("<p>" + newContent + "</p>");
+         }
+         /* This is how my book does it but I can't get it to work
+            document.getElementById("content").innerHTML = newContent;
+         */
+      }
+   };
+                  // url would go here
+   xhr.open("GET", "js/test.json", true);
+   xhr.send(null);
