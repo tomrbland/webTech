329cf7171868f8c15395ec7719ffa08827c06315
AK <ak15308@my.bristol.ac.uk>
2016-02-20 19:08:55 +0000

    Made a (very) small start on getting the drone strikes data feed.

---
+++ js/dronestream.js
@@ -0,0 +1,13 @@
+
+   document.write('<p>' + "Test." + '</p>');
+
+   var data={ "firstName" : "Hi Tom" };
+   alert(data.firstName);
+
+/*
+   This is the correct url for the dronestream
+   var url = api.dronestre.am/data
+
+   I think we'll need do this at some point.  
+   var obj = JSON.parse(text);
+*/
