0797236614b31b8ba99ec94441ef0591fcbf9713
AK <ak15308@my.bristol.ac.uk>
2016-02-21 23:34:09 +0000

    No real progress. I removed the CORS pkg from the project directory.

--- js/dronestream.js
+++ js/dronestream.js
@@ -1,19 +1,10 @@
 
-   /*  First attempt */
+   /*  First attempt (which worked on Safari w/o the server) */
 
-   var express = require('express')
-     , cors = require('cors')
-     , app = express();
-
-   app.use(cors());
-
-   app.get('/products/:id', function(req, res, next){
-     res.json({msg: 'This is CORS-enabled for all origins!'});
-   });
-
-   app.listen(80, function(){
-     console.log('CORS-enabled web server listening on port 80');
-   });
+   /* The CORS stuff I couldn't get working
+   var cors = require('cors');
+   var app = express()
+   app.use(cors()) */
 
    var url = "http://api.dronestre.am/data";
 
