af1ae5a5c8bbc4c1ba7efc2007a4d6315b2b8d5a
tomrbland <tomrbland@yahoo.co.uk>
2016-02-21 18:07:23 +0000

    drones 2. Just looks for the status. Doesn't seem to call the handler though. Not sure what's going on tbh

--- js/drones2.js
+++ js/drones2.js
@@ -4,40 +4,34 @@ addEventListener('load', start);
 
 function start()
 {
-   /*var newcontent = document.createElement('p');
-   newcontent.id = 'tomtest-content';
-   newcontent.appendChild(document.createTextNode('HERE IS SOME JS CONTENT'));
-
-   var scr = document.getElementById('tomtest');
-   scr.parentNode.insertBefore(newcontent, scr);*/
+   var pelement = document.createElement('p');
+   var textnode = document.createTextNode("TEST");
+   pelement.appendChild(textnode);
+   document.getElementById('test').appendChild(pelement);
 }
 
 var url = "http://api.dronestre.am/data";
-var xhr = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 
-xhr.onload = function() {
-   if (xhr.status === 200) {
-      var scr = document.getElementById('responseCheck');
-      
-      document.write("<p>" + "Response check." + "</p>");
-      responseObject = JSON.parse(xhr.responseText);
+request.addEventListener('load', drones);
 
-      // Build up the string with new content
-      var newContent = "<p>";
-      for (var i = 0; i < responseObject.strike.length; i++) {
-         // Parses the country of each drone strike event
-         newContent += " " + responseObject.strike[i].country + " ";
-      }
-      newContent += "</p>";
-      document.write(newContent);
-      /* This is how my book does it but I can't get it to work
-         document.getElementById("country").innerHTML = newContent;
-      */
+// replace url with "js/test.json" to see it working with the test file
+// true == asynchronous processing
+request.open("GET", url, true);
+request.send(null);
+
+function drones()
+{
+   if(request.status === 200){
+      var pelement = document.createElement('p');
+      var textnode = document.createTextNode("Response OK");
+      pelement.appendChild(textnode);
+      document.getElementById('responseCheck').appendChild(pelement);
    }
-   document.write("<p>" + "<a href='index.html'>" + "BACK" + "</a>" + "</p>");
-};
-
-               // replace url with "js/test.json" to see it working with the test file
-                     // true == asynchronous processing
-xhr.open("GET", url, true);
-xhr.send(null);
+   else{
+      var pelement = document.createElement('p');
+      var textnode = document.createTextNode("Response Not 200");
+      pelement.appendChild(textnode);
+      document.getElementById('responseCheck').appendChild(pelement);
+   }
+}
