80a08e05dc4bcae68f1f4af9d758635532624874
tomrbland <tomrbland@yahoo.co.uk>
2016-02-21 17:29:01 +0000

    push test - also building drones2

---
+++ js/drones2.js
@@ -0,0 +1,42 @@
+"use strict"
+
+addEventListener('load', start);
+
+function start()
+{
+   /*var newcontent = document.createElement('p');
+   newcontent.id = 'tomtest-content';
+   newcontent.appendChild(document.createTextNode('HERE IS SOME JS CONTENT'));
+
+   var scr = document.getElementById('tomtest');
+   scr.parentNode.insertBefore(newcontent, scr);*/
+}
+
+var url = "http://api.dronestre.am/data";
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+   if (xhr.status === 200) {
+      var scr = document.getElementById('responseCheck');
+      document.write("<p>" + "Response check." + "</p>");
+      responseObject = JSON.parse(xhr.responseText);
+
+      // Build up the string with new content
+      var newContent = "<p>";
+      for (var i = 0; i < responseObject.strike.length; i++) {
+         // Parses the country of each drone strike event
+         newContent += " " + responseObject.strike[i].country + " ";
+      }
+      newContent += "</p>";
+      document.write(newContent);
+      /* This is how my book does it but I can't get it to work
+         document.getElementById("country").innerHTML = newContent;
+      */
+   }
+   document.write("<p>" + "<a href='index.html'>" + "BACK" + "</a>" + "</p>");
+};
+
+               // replace url with "js/test.json" to see it working with the test file
+                     // true == asynchronous processing
+xhr.open("GET", url, true);
+xhr.send(null);
