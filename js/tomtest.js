d73dfecf97106fdf45830c570d91ee800cc4b858
tomrbland <tomrbland@yahoo.co.uk>
2016-02-21 17:00:29 +0000

    Updated tomtest to execute after load

--- js/tomtest.js
+++ js/tomtest.js
@@ -1,6 +1,13 @@
-var newcontent = document.createElement('p');
-newcontent.id = 'tomtest-content';
-newcontent.appendChild(document.createTextNode('HERE IS SOME JS CONTENT'));
+"use strict"
 
-var scr = document.getElementById('tomtest');
-scr.parentNode.insertBefore(newcontent, scr);
+addEventListener('load', start);
+
+function start()
+{
+   var newcontent = document.createElement('p');
+   newcontent.id = 'tomtest-content';
+   newcontent.appendChild(document.createTextNode('HERE IS SOME JS CONTENT'));
+
+   var scr = document.getElementById('tomtest');
+   scr.parentNode.insertBefore(newcontent, scr);
+}
