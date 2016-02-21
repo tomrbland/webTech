010bd5e884836d35b44d45b5442152487f537a0c
AK <ak15308@my.bristol.ac.uk>
2016-02-21 22:14:24 +0000

    Trying to sort out the CORS issue

--- js/drones2.js
+++ js/drones2.js
@@ -1,37 +1,37 @@
-"use strict"
+   "use strict";
 
-addEventListener('load', start);
+   addEventListener("load", start);
 
-function start()
-{
-   var pelement = document.createElement('p');
-   var textnode = document.createTextNode("TEST");
-   pelement.appendChild(textnode);
-   document.getElementById('test').appendChild(pelement);
-}
-
-var url = "http://api.dronestre.am/data";
-var request = new XMLHttpRequest();
-
-request.addEventListener('load', drones);
-
-// replace url with "js/test.json" to see it working with the test file
-// true == asynchronous processing
-request.open("GET", url, true);
-request.send(null);
-
-function drones()
-{
-   if(request.status === 200){
-      var pelement = document.createElement('p');
-      var textnode = document.createTextNode("Response OK");
+   function start()
+   {
+      var pelement = document.createElement("p");
+      var textnode = document.createTextNode("TEST");
       pelement.appendChild(textnode);
-      document.getElementById('responseCheck').appendChild(pelement);
+      document.getElementById("test").appendChild(pelement);
    }
-   else{
-      var pelement = document.createElement('p');
-      var textnode = document.createTextNode("Response Not 200");
-      pelement.appendChild(textnode);
-      document.getElementById('responseCheck').appendChild(pelement);
+
+   var url = "http://api.dronestre.am/data";
+   var request = new XMLHttpRequest();
+
+   request.addEventListener("load", drones);
+
+   function drones()
+   {
+      if(request.status === 200){
+         var pelement = document.createElement("p");
+         var textnode = document.createTextNode("Response OK");
+         pelement.appendChild(textnode);
+         document.getElementById("responseCheck").appendChild(pelement);
+      }
+      else{
+         var pelement = document.createElement("p");
+         var textnode = document.createTextNode("Response Not 200");
+         pelement.appendChild(textnode);
+         document.getElementById("responseCheck").appendChild(pelement);
+      }
    }
-}
+
+   // replace url with "js/test.json" to see it working with the test file
+   // true == asynchronous processing
+   request.open("GET", url, true);
+   request.send(null);
--- js/dronestream.js
+++ js/dronestream.js
@@ -1,4 +1,4 @@
-
+/*
    var url = "http://api.dronestre.am/data";
 
    var xhr = new XMLHttpRequest();
@@ -19,12 +19,77 @@
          document.write(newContent);
          /* This is how my book does it but I can't get it to work
             document.getElementById("country").innerHTML = newContent;
-         */
+
       }
       document.write("<p>" + "<a href='index.html'>" + "BACK" + "</a>" + "</p>");
    };
 
-                  // replace url with "js/test.json" to see it working with the test file
-                        // true == asynchronous processing
+                   replace url with "js/test.json" to see it working with the test file
+                        true == asynchronous processing
    xhr.open("GET", url, true);
    xhr.send(null);
+*/
+
+   function createCORSRequest(method, url) {
+       var xhr = new XMLHttpRequest();
+       if ("withCredentials" in xhr){
+           xhr.open(method, url, true);
+       }
+       else if (typeof XDomainRequest !== "undefined"){
+           xhr = new XDomainRequest();
+           xhr.open(method, url);
+       }
+       else {
+           xhr = null;
+       }
+       return xhr;
+   }
+
+   var request = createCORSRequest("GET", "http://api.dronestre.am/data");
+   if (request) {
+       request.onload = function() {
+          if (xhr.status === 200) {
+             document.write("<p>" + "Response check." + "</p>");
+             responseObject = JSON.parse(xhr.responseText);
+
+             // Build up the string with new content
+             var newContent = "<p>";
+             for (var i = 0; i < responseObject.strike.length; i++) {
+                // Parses the country of each drone strike event
+                newContent += " " + responseObject.strike[i].country + " ";
+             }
+             newContent += "</p>";
+             document.write(newContent);
+          }
+     };
+
+   }
+    request.send();
+
+
+
+/*
+   "use strict";
+
+   var url = "http://api.dronestre.am/data";
+
+   var request = new XMLHttpRequest();
+   request.onreadystatechange = receive;
+   request.open("GET", url, true);
+   request.send(null);
+
+   function receive() {
+      if (this.readyState != XMLHttpRequest.DONE) {
+         var pelement = document.createElement("p");
+         var textnode = document.createTextNode("Response not OK");
+         pelement.appendChild(textnode);
+         document.getElementById("responseCheck").appendChild(pelement);
+      }
+      else {
+         var pelement = document.createElement("p");
+         var textnode = document.createTextNode("Response OK");
+         pelement.appendChild(textnode);
+         document.getElementById("responseCheck").appendChild(pelement);
+      }
+   }
+*/
