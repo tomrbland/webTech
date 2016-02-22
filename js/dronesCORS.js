cd0b14031864e89d646cb23a6803f4c3b5dde92f
tomrbland <tomrbland@yahoo.co.uk>
2016-02-22 12:13:52 +0000

    Tidied and got rid of the CORS header function

--- js/dronesCORS.js
+++ js/dronesCORS.js
@@ -2,24 +2,9 @@
 
    addEventListener("load", start);
 
-   function start()
-   {
-      var pelement = document.createElement("p");
-      var textnode = document.createTextNode("TEST");
-      pelement.appendChild(textnode);
-      document.getElementById("test").appendChild(pelement);
-   }
-
    var url = "http://api.dronestre.am/data";
-   //var request = new XMLHttpRequest(true, true);
+   var request = new XMLHttpRequest(true, true);
 
-   var request = createCORSRequest('GET', url);
-   if (!request) {
-     throw new Error('CORS not supported');
-   }
-
-
-   //request.addEventListener("load", drones);
    request.onreadystatechange = drones;
 
    // replace url with "js/test.json" to see it working with the test file
@@ -27,42 +12,36 @@
    request.open("GET", url, true);
    request.send(null);
 
+
+//FUNCTIONS--------------------------------
+
+   function start()
+   {
+      printArea("Test");
+   }
+
+   function printArea(s)
+   {
+      var pelement = document.createElement("p");
+      var textnode = document.createTextNode(s);
+      pelement.appendChild(textnode);
+      document.getElementById("test").appendChild(pelement);
+   }
+
    function drones()
    {
       if(request.status === 200){
-         var pelement = document.createElement("p");
-         var textnode = document.createTextNode("Response OK");
-         pelement.appendChild(textnode);
-         document.getElementById("responseCheck").appendChild(pelement);
+         printResponse("Response OK");
       }
       else{
-         var pelement = document.createElement("p");
-         var textnode = document.createTextNode("Response Not 200. is: " + request.status);
-         pelement.appendChild(textnode);
-         document.getElementById("responseCheck").appendChild(pelement);
+         printResponse("Response Not 200. is: " + request.status);
       }
    }
 
-   function createCORSRequest(method, url) {
-     var xhr = new XMLHttpRequest();
-     if ("withCredentials" in xhr) {
-
-       // Check if the XMLHttpRequest object has a "withCredentials" property.
-       // "withCredentials" only exists on XMLHTTPRequest2 objects.
-       xhr.open(method, url, true);
-
-     } else if (typeof XDomainRequest != "undefined") {
-
-       // Otherwise, check if XDomainRequest.
-       // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-       xhr = new XDomainRequest();
-       xhr.open(method, url);
-
-     } else {
-
-       // Otherwise, CORS is not supported by the browser.
-       xhr = null;
-
-     }
-     return xhr;
+   function printResponse(s)
+   {
+      var pelement = document.createElement("p");
+      var textnode = document.createTextNode(s);
+      pelement.appendChild(textnode);
+      document.getElementById("responseCheck").appendChild(pelement);
    }
