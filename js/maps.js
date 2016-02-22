91ea5df594e0e0267da9714e94b1b63f02f41a1d
tomrbland <tomrbland@yahoo.co.uk>
2016-02-22 20:35:10 +0000

    Now implmeneted properly. Works!

--- js/maps.js
+++ js/maps.js
@@ -1,18 +1,8 @@
-//var map;
+var map;
 
-/*function initMap() {
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
-}*/
-
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
