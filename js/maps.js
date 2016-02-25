565349d9b8776d0244731e0f66f7d44031c03f3b
tomrbland <tomrbland@yahoo.co.uk>
2016-02-25 21:22:08 +0000

    Added a pin a message above Les Arcs

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,32 @@
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.69, lng: 6.78},
-    zoom: 4
 
-  });
-  webcamstravel.easymap.load(map);
+   var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.69, lng: 6.78},
+      zoom: 4
+   });
+
+   var lesarcs = {lat: 45.572584, lng: 6.820364};
+   var lesarcs_content = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Les Arcs!</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Les Arcs</b>, is here! Let\'s vote!</p>'+
+      '</div>'+
+      '</div>';
+
+   var infowindow = new google.maps.InfoWindow({content: lesarcs_content});
+   var marker = new google.maps.Marker({
+                                        position: lesarcs,
+                                        map: map,
+                                        title: 'Les Arcs'
+                                      });
+
+   marker.addListener('click', function() {
+      infowindow.open(map, marker);
+   });
+
+   webcamstravel.easymap.load(map);
 }
