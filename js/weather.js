f1fdc331a735cd2e394f0b4761192301f675ceba
AK <ak15308@my.bristol.ac.uk>
2016-03-01 01:29:06 +0000

    Have managed to parse the data in lots of different ways - uncomment my console.log() calls to see.
    Two questions:
    1. Why does it print out undefined at the top (like in dronestream.js)?
    2. Why are there more than 24 hourly weather summaries?

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ JSON.stringify turns a Javascript object into JSON text and stores that JSON tex
 JSON.parse turns a string of JSON text into a Javascript object.
 */
 
-/* THIS WORKS */
+/* THIS WORKS
    var ForecastIo = require('forecastio');
    // The forecast for London!
    var forecastIo = new ForecastIo('885a85ff4be082090e0348da41f005dd');
@@ -11,31 +11,34 @@ JSON.parse turns a string of JSON text into a Javascript object.
 
    console.log(JSON.stringify(data, null, 2));
    });
+*/
 
 
-
-/* TRYING TO PARSE...
+/* PARSING EXPERIMENTS... */
    var ForecastIo = require('forecastio');
-
-   var jsonString, jsObject;
 
    // The forecast for London!
    var forecastIo = new ForecastIo('885a85ff4be082090e0348da41f005dd');
    forecastIo.forecast('51.506', '-0.127').then(function(data) {
-      // Turns it into a JS object (like in dronestream).
-      jsonString = JSON.stringify(data, null, 2);
+
+      var jsonString = JSON.stringify(data, null, 2);
+      //console.log(jsonString);
+
+      var jsObject = JSON.parse(jsonString);
+      //console.log(jsObject);
+
+      var newContent;
+
+      //console.log(jsObject.currently);
+      //console.log(jsObject.minutely.data[2]);
+
+      for (var i = 0; i < jsObject.hourly.data.length; i++) {
+         // Builds a string to print via concatenation.
+         newContent += "\n" + jsObject.hourly.data[i].summary;
+      }
+      console.log(newContent);
    });
 
-   jsObject = JSON.parse(jsonString);
-
-   var newContent;
-   // Parses the data
-   for (var i = 0; i < jsObject.data.length; i++) {
-      // Builds a string to print via concatenation.
-      newContent += " " + jsObject.data[i] + " ";
-   }
-   console.log(newContent);
-*/
 
 
    /*
