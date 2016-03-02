364e17ef3153e6856e0d951f4b0c632cc363ca91
tomrbland <tomrbland@yahoo.co.uk>
2016-03-02 16:07:21 +0000

    Nav bar now loads using js, ready to be linked to a db of pages

---
+++ js/navigation.js
@@ -0,0 +1,23 @@
+"use strict"
+
+addEventListener('load', addNavigation);
+
+function addNavigation(){
+
+   var navBar = document.querySelector('#nav');
+
+   navBar.appendChild(makeDiv('index.html', 'Home'));
+   navBar.appendChild(makeDiv('resort.html', 'Resort Page'));
+
+   function makeDiv(path, text){
+      var div = document.createElement('div');
+      div.className = 'navButton';
+
+      var a = document.createElement('a');
+      a.href = path;
+      a.innerHTML = text;
+
+      div.appendChild(a);
+      return div;
+   }
+}
