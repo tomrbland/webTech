2bf8b428dfea000f4eebee18ae0dd24eeef28993
tomrbland <tomrbland@yahoo.co.uk>
2016-03-02 17:20:49 +0000

    Resort name now has space rather than +

--- js/resortHeader.js
+++ js/resortHeader.js
@@ -3,11 +3,12 @@ addEventListener("load", buildHeader);
 
 function buildHeader() {
    var header = document.querySelector("#resort");
-   var h1 = document.createElement('h2');
+   var h2 = document.createElement('h2');
 
    var data = window.location.search.replace("?", "");
+   data = data.replace('+', ' ');
    var parts = data.split("=");
    h1.innerHTML = parts[1];
 
-   header.appendChild(h1);
+   header.appendChild(h2);
 }
