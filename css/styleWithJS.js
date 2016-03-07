900edffe8441078dc1f2993dc58049bed68edf79
tomrbland <tomrbland@yahoo.co.uk>
2016-03-07 18:24:53 +0000

    more tweaks

--- css/styleWithJS.js
+++ css/styleWithJS.js
@@ -29,13 +29,16 @@ function size(){
    responsive.style.height = setHeight + 'px';
 
    function getHeight(o){
-      var ptop = parseInt(window.getComputedStyle(o, null).getPropertyValue("padding-top"), 10);
+      /*var ptop = parseInt(window.getComputedStyle(o, null).getPropertyValue("padding-top"), 10);
       var pbottom = parseInt(window.getComputedStyle(o,null).getPropertyValue("padding-bottom"),10);
       var mtop = parseInt(window.getComputedStyle(o,null).getPropertyValue("margin-top"),10);
       var mbottom = parseInt(window.getComputedStyle(o,null).getPropertyValue("margin-bottom"),10);
-      var height = parseInt(window.getComputedStyle(o, null).getPropertyValue('height'), 10);
-      return ptop + pbottom + mtop + mbottom + height;
+      var height = parseInt(window.getComputedStyle(o, null).getPropertyValue('height'), 10);*/
+
+      return o.offsetHeight;
+      //return ptop + pbottom + mtop + mbottom + height;
    }
+
    function reorderDivs(width){
       var photos = document.querySelector('#photos');
       var weather = document.querySelector('#weather');
