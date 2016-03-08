25033e9c400f5afa3b917da3bd80311401a470da
AK <ak15308@my.bristol.ac.uk>
2016-03-08 01:24:12 +0000

    cleaned up resortHeader.js

--- js/resortHeader.js
+++ js/resortHeader.js
@@ -1,17 +1,22 @@
-"use strict";
+"use strict"
 
-addEventListener("load", buildHeader);
+if (addEventListener) {
+   addEventListener("load", buildHeader);
+}
+else {
+   attachEvent("onload", buildHeader);
+}
 var resortName;
 
 function buildHeader() {
    console.log("resortHeader.js: buildHeader - event on load");
    var header = document.querySelector("#resort");
-   var h2 = document.createElement('h2');
+   var h2 = document.createElement("h2");
 
    var data = window.location.search.replace("?", "");
    data = decodeURI(data);
-   data = data.split('+').join(' ');
-   data = data.split('%2C').join(',');
+   data = data.split("+").join(" ");
+   data = data.split("%2C").join(",");
    var parts = data.split("=");
    resortName = parts[1];
    console.log("resortHeader.js: Global resortName set");
