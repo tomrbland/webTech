e3ba9bb4499968141713303c1e214bb7cf55cb4a
tomrbland <tomrbland@yahoo.co.uk>
2016-05-19 12:48:29 +0100

    Simple review writer. Needs to bring up a login page if user is not logged in. Also needs to submit it's data somewhere

--- js/pagelayout/addreviewbutton.js
+++ js/pagelayout/addreviewbutton.js
@@ -4,19 +4,18 @@ addEventListener("load", connectButton);
 
 function connectButton(){
    var button = document.querySelector("#addReviewButton");
-   var close = document.querySelector("#closeButton");
+   var close = document.querySelector(".closeWriteReview");
    button.addEventListener("click", clicked);
    close.addEventListener("click", closeAddReview);
 }
 
 function clicked(){
-   console.log("CLICKED");
+   //IF NOT LOGGED IN REDIRECT TO LOGIN? or open a login pop-up (nicer UX?)?
    var button = document.querySelector("#dim");
    button.style.display = "block";
 }
 
 function closeAddReview(){
-   console.log("CLICKED");
    var button = document.querySelector("#dim");
    button.style.display = "none";
 }
