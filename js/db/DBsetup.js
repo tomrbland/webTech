08e8d07cf5a449ba20f94dcfaf606b5b66311a4d
AK <ak15308@my.bristol.ac.uk>
2016-05-19 14:11:24 +0100

    added title to the review table and inserted data.

--- js/db/DBsetup.js
+++ js/db/DBsetup.js
@@ -29,6 +29,7 @@ function startup() {
       "CREATE TABLE Review (" +
          "id INTEGER PRIMARY KEY, " +
          "personid INTEGER UNIQUE NOT NULL REFERENCES Person (id), " +
+         "title TEXT NOT NULL, " +
          "text TEXT NOT NULL" +
       ")",
    err);
@@ -39,17 +40,18 @@ function startup() {
   db.run("INSERT INTO Person (username) VALUES ('NoviceSkiier');", err);
   db.run("INSERT INTO Person (username) VALUES ('BoarderDude');", err);
 
-  db.run("INSERT INTO Review (personid, text) VALUES (1, 'Excellent resort. The reindeers also liked it.');", err);
-  db.run("INSERT INTO Review (personid, text) VALUES (2, 'I wasn''t really paying attention to the resort - I was too busy jumping over helicopters!');", err);
-  db.run("INSERT INTO Review (personid, text) VALUES (3, 'There were too many bad guys chasing me in this resort: 2/10.');", err);
-  db.run("INSERT INTO Review (personid, text) VALUES (4, 'I had a great time learning how to ski here.');", err);
-  db.run("INSERT INTO Review (personid, text) VALUES (5, 'It had a great halfpipe.');", err);
+  db.run("INSERT INTO Review (personid, title, text) VALUES (1, 'We all had a merry time.', 'Excellent resort. The reindeers also liked it.');", err);
+  db.run("INSERT INTO Review (personid, title, text) VALUES (2, 'A good location for my stunts.', 'I wasn''t really paying attention to the resort - I was too busy jumping over helicopters!');", err);
+  db.run("INSERT INTO Review (personid, title, text) VALUES (3, 'No matter where I go I seem to make enemies...', 'There were too many bad guys chasing me in this resort: 2/10.');", err);
+  db.run("INSERT INTO Review (personid, title, text) VALUES (4, 'A good resort for novices.', 'I had a great time learning how to ski here.');", err);
+  db.run("INSERT INTO Review (personid, title, text) VALUES (5, 'Rad.', 'It had a sweet halfpipe.');", err);
 
   db.close();
 }
 
 function err(e) {
    if (e) {
+      console.log("ERROR");
       throw e;
    }
 }
