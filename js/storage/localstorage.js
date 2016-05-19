b8e4618d36ee73f3006e5a0ad4f470fc6c1ce46b
tomrbland <tomrbland@yahoo.co.uk>
2016-05-19 20:29:59 +0100

    Added remove functions

--- js/storage/localstorage.js
+++ js/storage/localstorage.js
@@ -4,6 +4,10 @@ function store(name, s){
    localStorage.setItem(name, s);
 }
 
-function get(name, s){
+function get(name){
    return localStorage.getItem(name);
+}
+
+function remove(name){
+   return localStorage.removeItem(name);
 }
--- js/storage/sessionstorage.js
+++ js/storage/sessionstorage.js
@@ -4,6 +4,10 @@ function store(name, s){
    sessionStorage.setItem(name, s);
 }
 
-function get(name, s){
+function get(name){
    return sessionStorage.getItem(name);
+}
+
+function remove(name){
+   return sessionStorage.removeItem(name);
 }
