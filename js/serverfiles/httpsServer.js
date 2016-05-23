17f220d123f2127d32482d65e5ff9f499d5833f4
AK <ak15308@my.bristol.ac.uk>
2016-05-23 16:26:02 +0100

    experimenting with how to close while keeping a modular structure

--- js/serverfiles/httpsServer.js
+++ js/serverfiles/httpsServer.js
@@ -12,8 +12,29 @@
    module.exports = {
       start: function(port){
          _start(port);
+      },
+
+      service: function(){
+         _service();
+      },
+
+      shutDown: function(){
+         _shutDown(service, myCallback);
       }
    };
+
+   /*
+   //Exports
+   module.exports = {
+      reply: function(response, url, type){
+         _reply(response, url, type);
+      },
+
+      fail: function(response, code, text){
+         _fail(response, code, text);
+      }
+   };
+   */
 
 //Code
    //start(8443);
@@ -26,21 +47,53 @@
       }
 
       var service = HTTP.createServer(credentials, URL_VALIDATION.validate);
-//
-      service.on('close', function() {
-        console.log(' Stopping ...');
-     });
-
-      process.on('SIGINT', function() {
-         process.exit(2);
-         service.close();
-     });
-
-     process.on('exit', function () {
-      // process.emit('cleanup');
-      console.log("hi");
-     });
 
       service.listen(port, "localhost");
       console.log("Visit https://localhost:" + port);
+
+      function _service() {
+         return service;
+      }
    }
+
+   HTTPS_SERVER.shutdown(HTTPS_SERVER.service(), db.close())
+
+
+   function _shutDown(service, funct) {
+      funct();
+      console.log("\nServer connection closed.");
+      service.close(function exitProcess() {
+         process.exit();
+      });
+   }
+
+   //Listens for the kill command
+   process.on("SIGTERM", _shutDown);
+
+   //Listens for the Ctrl-C command
+   process.on("SIGINT", _shutDown);
+/*
+   function shutDown() {
+      //close db
+      console.log("\nServer connection closed.");
+      service.close(function exitProcess() {
+         process.exit();
+      });
+   } */
+
+// WHY ISNT THIS WORKING
+// !!!!
+/*
+   service.on('close', function() {
+     console.log(' Stopping ...');
+  });
+
+   process.on('SIGINT', function() {
+      process.exit(2);
+      service.close();
+  });
+
+  process.on('exit', function () {
+   // process.emit('cleanup');
+   console.log("hi");
+}); */
