866d46f59848488499b738b27d92653d7c06ef96
AK <ak15308@my.bristol.ac.uk>
2016-05-25 20:58:07 +0100

    first commit

--- js/pagelayout/addreviewbutton.js
+++ js/pagelayout/addreviewbutton.js
@@ -1,51 +1,51 @@
-"use strict";
+   "use strict";
 
-addEventListener("load", connectButton);
+   addEventListener("load", connectButton);
 
-function connectButton(){
-   var button = document.querySelector("#addReviewButton");
-   var close = document.querySelector(".closeWriteReview");
-   var uid = sessionStorage.getItem("uid");
-   var username = sessionStorage.getItem("username");
-   button.addEventListener("click", amILoggedIn);
-   close.addEventListener("click", closeAddReview);
-   completeForm();
+   function connectButton(){
+      var button = document.querySelector("#addReviewButton");
+      var close = document.querySelector(".closeWriteReview");
+      var userSessionID = sessionStorage.getItem("userSessionID");
+      var username = sessionStorage.getItem("username");
+      button.addEventListener("click", amILoggedIn);
+      close.addEventListener("click", closeAddReview);
+      completeForm();
 
-   function closeAddReview(){
-      var button = document.querySelector("#dim");
-      button.style.display = "none";
-   }
+      function closeAddReview(){
+         var button = document.querySelector("#dim");
+         button.style.display = "none";
+      }
 
-   function amILoggedIn(){
+      function amILoggedIn(){
 
-      /*// Uncomment to bypass login check
-      var reviewWriter = document.querySelector("#dim");
-      reviewWriter.style.display = "block";*/
+         /*// Uncomment to bypass login check
+         var reviewWriter = document.querySelector("#dim");
+         reviewWriter.style.display = "block";*/
 
-      var xhr = new XMLHttpRequest();
-      var data = "uid="+uid+"&username="+username;
+         var xhr = new XMLHttpRequest();
+         var data = "userSessionID="+userSessionID+"&username="+username;
 
-      xhr.open("POST", "js/db/amILoggedIn", true);
-      xhr.onreadystatechange = gotReply;
-      xhr.send(data);
-   }
+         xhr.open("POST", "js/db/amILoggedIn", true);
+         xhr.onreadystatechange = gotReply;
+         xhr.send(data);
+      }
 
-   function gotReply(){
-      if(this.readyState == 4 && this.status == 200) {
-         if(this.responseText === "ok"){
-            var reviewWriter = document.querySelector("#dim");
-            reviewWriter.style.display = "block";
-         }
-         else{
-            window.location.href="/login.html";
+      function gotReply(){
+         if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText === "Yes") {
+               var reviewWriter = document.querySelector("#dim");
+               reviewWriter.style.display = "block";
+            }
+            else {
+               window.location.href="/login.html";
+            }
          }
       }
-   }
 
-   function completeForm(){
-      var formUID = document.querySelector("#formuid");
-      var formUsername = document.querySelector("#formusername");
-      formUID.value = uid;
-      formUsername.value = username;
+      function completeForm(){
+         var formUID = document.querySelector("#formuid");
+         var formUsername = document.querySelector("#formusername");
+         formUID.value = userSessionID;
+         formUsername.value = username;
+      }
    }
-}
