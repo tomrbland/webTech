48a64b937bcd8e3087dc58e8c20e619488fa842b
AK <ak15308@my.bristol.ac.uk>
2016-05-26 19:40:37 +0100

    made pedantic change

--- js/loginfailed.js
+++ js/loginfailed.js
@@ -1,19 +1,19 @@
-"use strict";
+   "use strict";
 
-addEventListener("load", didLoginFail);
+   addEventListener("load", didLoginFail);
 
-function didLoginFail(){
-   var submittedFrom = sessionStorage.getItem("formsubmittedfrom");
+   function didLoginFail(){
+      var submittedFrom = sessionStorage.getItem("formsubmittedfrom");
 
-   var data = window.location.search.replace("?", "");
-   data = decodeURI(data);
-   data = data.split("+").join(" ");
-   data = data.split("%2C").join(",");
-   var parts = data.split("=");
-   var state = parts[1];
-   if(state.indexOf("fail") > -1){
-      if(submittedFrom.indexOf("login") > -1)
-         alert("Login Failed");
-      else alert("Registration failed.\nUsername must be unique.");
+      var data = window.location.search.replace("?", "");
+      data = decodeURI(data);
+      data = data.split("+").join(" ");
+      data = data.split("%2C").join(",");
+      var parts = data.split("=");
+      var state = parts[1];
+      if(state.indexOf("fail") > -1){
+         if(submittedFrom.indexOf("login") > -1)
+            alert("Login Failed.");
+         else alert("Registration failed.\nUsername must be unique.");
+      }
    }
-}
